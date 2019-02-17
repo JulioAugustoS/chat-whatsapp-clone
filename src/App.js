8463b34af94d19cc06efd78358532c3042b6b553
Julio Augusto <julio@lapse.com.br>
2019-02-17 03:02:45 -0300

    Adicionado firebase e redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import firebase from 'firebase';
 
 import store from './store';
 import Routes from './Routes';
+import { config } from './utils/firebase-config';
 
-const App = props => (
-  <Provider store={store}>
-    <Routes />
-  </Provider>
-);
+class App extends Component {
+  componentDidMount(){
+    // Initialize Firebase
+    firebase.initializeApp(config);
+  }
+
+  render(){
+    return (
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    );
+  }
+};
 
 export default App;
---
+++ src/utils/firebase-config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyA1oy55YtP86kuMHdAd3fZTQf2k6bvIDUM",
+  authDomain: "whatsapp-clone-19d47.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-19d47.firebaseio.com",
+  projectId: "whatsapp-clone-19d47",
+  storageBucket: "whatsapp-clone-19d47.appspot.com",
+  messagingSenderId: "547024879472"
+};
