12165b9ca8a0a97ad6d36f2073cb73a6ecdad808
Julio Augusto <julio@lapse.com.br>
2019-02-18 13:29:27 -0300

    Adicionado nova View - Home

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,6 +4,7 @@ import { Router, Stack, Scene } from 'react-native-router-flux';
 // Views
 import Authentication from '../views/Authentication/Login';
 import Welcome from '../views/Initial/Welcome';
+import Home from '../views/Home';
 
 const Routes = props => {
   return (
@@ -11,6 +12,7 @@ const Routes = props => {
       <Stack key="root">
         <Scene key="authentication" component={Authentication} hideNavBar />
         <Scene key="welcome" component={Welcome} hideNavBar />
+        <Scene key="home" component={Home} />
       </Stack>
     </Router>
   )
---
+++ src/views/Home/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+class Home extends Component {
+  render(){
+    return (
+      <View style={{ marginTop: 100 }}>
+        <Text>Home</Text>
+      </View>
+    )
+  }
+}
+
+export default Home;
